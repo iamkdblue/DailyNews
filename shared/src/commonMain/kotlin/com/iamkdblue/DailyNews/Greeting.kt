e3e8190aa9edebf0576ba696cfe32f4a0fd5af95
kmourya <kuldeep.mourya@agoda.com>
2024-02-11 21:22:42 +0700

    move to compose multiplatform

--- shared/src/commonMain/kotlin/com/iamkdblue/DailyNews/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.iamkdblue.DailyNews
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
